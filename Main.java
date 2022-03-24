3ce402bdd37183a1eab0e44e4d5d7d56549d9a47
lanloptele <100698914+lanloptele@users.noreply.github.com>
2022-03-24 19:36:55 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    /*
+      *
+      **
+      ***
+      ****
+      *****
+      */
+    int i=0;
+    while(i<5){
+      i++;
+      for(int y=0;y<i;y++)
+        System.out.println("*");// do wczytnia znaku nie daje sie +
+        System.out.println();
+
+      }
+    }
+    
+  }
